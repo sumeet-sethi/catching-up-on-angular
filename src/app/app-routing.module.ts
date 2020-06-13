f89e80810124242317ccc86121a1d4c3fc9d802a
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2020-06-13 19:38:28 -0400

    Initial Angular9

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+
+
+const routes: Routes = [
+  {path: 'comp1', component: Comp1Component},
+  {path: 'comp2', component: Comp2Component},
+  {path: 'comp3', component: Comp3Component},
+  { path: '',   redirectTo: '/comp1', pathMatch: 'full' },
+  { path: '**', component: Comp3Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
+import { Comp3Component } from './comp3/comp3.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Comp1Component,
+    Comp2Component,
+    Comp3Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
