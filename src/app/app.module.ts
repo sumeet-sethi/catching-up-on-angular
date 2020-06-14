4d7cdfc210ca7b72fc6723962b66fa59dc9839b7
Sumeet Sethi <sumeetsethi.asm@gmail.com>
2020-06-13 20:01:52 -0400

    Adding Angular Material visuals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { Comp1Component } from './comp1/comp1.component';
 import { Comp2Component } from './comp2/comp2.component';
 import { Comp3Component } from './comp3/comp3.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,9 @@ import { Comp3Component } from './comp3/comp3.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, MatIconModule, MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
